590d2ddaabd827f1c853cf2ff5432dc062ae132f
Tonexus <ual_ynot@yahoo.com>
2018-08-09 18:10:28 -0700

    Updated to new API

--- stocks.py
+++ stocks.py
@@ -1,14 +1,18 @@
 import sys
 import configparser
+import collections
 
+import requests
 import numpy as np
-import quandl
 
 SOURCE = 'WIKI'
 DAYS = 5
 
 CFG_PATH = 'stocks.cfg'
 CSV_PATH = 'stocks.csv'
+
+API_URL = 'https://www.alphavantage.co/query?'
+QUERY = 'function=TIME_SERIES_DAILY&symbol={}&apikey={}'
 
 def read_csv(path):
     try:
@@ -27,7 +31,7 @@ if __name__ == '__main__':
     else:
         config = configparser.ConfigParser()
         config.read(CFG_PATH)
-        quandl.ApiConfig.api_key = config['api']['api key']
+        api_key = config['api']['api key']
         csv_stocks = read_csv(CSV_PATH)
         total_dollars = float(args[0])
         ratio_total = sum([int(x[1]) for x in csv_stocks])
@@ -37,13 +41,22 @@ if __name__ == '__main__':
         invest_shares = []
 
         for stock, ratio in csv_stocks:
-            _, _, hi, lo, _, _, _, _, _, _, _, _, _ = zip(*quandl.get('%s/%s' % (SOURCE, stock), rows=DAYS, returns='numpy'))
-            mean_price = np.mean(hi + lo)
-            dollars = total_dollars * int(ratio) / ratio_total
-            print('%s averaged $%0.2f per share over the last %d %s.' % (stock, mean_price, DAYS, 'day' if DAYS == 1 else 'days'))
-            invest_names.append(stock)
-            invest_dollars.append(dollars)
-            invest_shares.append(dollars / mean_price)
+            try:
+                raw_data = requests.get(API_URL + QUERY.format(stock, api_key))
+                stock_data = list(raw_data.json(object_pairs_hook=collections.OrderedDict)['Time Series (Daily)'].values())[:DAYS]
+                # print(list(stock_data['Time Series (Daily)'].keys())[:DAYS])
+                # bee = list(stock_data['Time Series (Daily)'].values())[:DAYS]
+                hi = np.fromiter((float(x['2. high']) for x in stock_data), np.float_)
+                lo = np.fromiter((float(x['3. low']) for x in stock_data), np.float_)
+                mean_price = (np.mean(hi) + np.mean(lo)) / 2
+                dollars = total_dollars * int(ratio) / ratio_total
+                print('%s averaged $%0.2f per share over the last %d %s.' % (stock, mean_price, DAYS, 'day' if DAYS == 1 else 'days'))
+                invest_names.append(stock)
+                invest_dollars.append(dollars)
+                invest_shares.append(dollars / mean_price)
+            except KeyError as e:
+                print('Error Occurred:')
+                print(raw_data)
 
         print('\nDollar Cost Averaging Advice:')
 
